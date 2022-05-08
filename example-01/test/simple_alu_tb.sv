38652ab70f87bba9416059f62bb41b823a911743
Jan Marjanovic <jan.marjanovic@outlook.com>
2022-05-08 22:35:22 +0200

    example-01: add a sequencer and a test

--- example-01/test/simple_alu_tb.sv
+++ example-01/test/simple_alu_tb.sv
@@ -23,10 +23,19 @@ endinterface
 //==============================================================================
 // drivers
 
-typedef struct {
-    bit [7:0] a, b;
-    bit       op;
-} t_data;
+class t_data extends uvm_sequence_item;
+    rand bit [7:0] a, b;
+    rand bit       op;
+
+    function new(string name = "t_data");
+        super.new(name);
+    endfunction
+
+    `uvm_object_utils_begin( t_data )
+        `uvm_field_int ( a, UVM_ALL_ON )
+        `uvm_field_int ( b, UVM_ALL_ON )
+    `uvm_object_utils_end
+endclass
 
 
 class in_if_driver extends uvm_driver #(t_data);
@@ -35,23 +44,33 @@ class in_if_driver extends uvm_driver #(t_data);
 
     `uvm_component_utils(in_if_driver)
 
-    // Constructor
     function new (string name, uvm_component parent);
         super.new(name, parent);
     endfunction : new
 
-
     virtual task run_phase(uvm_phase phase);
-    //forever begin
+    forever begin
         `uvm_info("driver", "`run` phase", UVM_LOW);
-        //seq_item_port.get_next_item(req);
+        seq_item_port.get_next_item(req);
         //respond_to_transfer(req);
-        //drive();
-        //seq_item_port.item_done();
-    //end
+        // TODO: drive();
+        seq_item_port.item_done();
+    end
     endtask : run_phase
 
 endclass : in_if_driver
+
+
+//==============================================================================
+// sequencer
+
+class simple_alu_seqr extends uvm_sequencer #(t_data);
+    `uvm_component_utils(simple_alu_seqr)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+endclass
 
 
 //==============================================================================
@@ -61,8 +80,9 @@ class env extends uvm_env;
 
     `uvm_component_utils(env)
 
-    virtual simple_alu_in_if in_if;
-    in_if_driver             drv;
+    virtual simple_alu_in_if m_in_if;
+    in_if_driver             m_drv;
+    simple_alu_seqr          m_seqr;
 
     function new(string name, uvm_component parent = null);
         super.new(name, parent);
@@ -72,15 +92,15 @@ class env extends uvm_env;
     function void build_phase(uvm_phase phase);
         `uvm_info("env", "In the `build` phase", UVM_LOW);
         super.build_phase(phase);
-        drv = in_if_driver::type_id::create("drv", this);
-        // seqr = uvm_sequencer#(t_data)::type_id::create("seqr", this);
+        m_drv = in_if_driver::type_id::create("drv", this);
+        m_seqr = simple_alu_seqr::type_id::create("seqr", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
         `uvm_info("env", "In the `connect` phase", UVM_LOW);
         assert(uvm_resource_db#(virtual simple_alu_in_if)::read_by_name(
-                get_full_name(), "simple_alu_in_if", in_if));
-        // drv.seq_item_port.connect(seqr.seq_item_export);
+                get_full_name(), "simple_alu_in_if", m_in_if));
+        m_drv.seq_item_port.connect(m_seqr.seq_item_export);
     endfunction
 
     task run_phase(uvm_phase phase);
@@ -90,6 +110,59 @@ class env extends uvm_env;
     endtask
 endclass
 
+//==============================================================================
+// sequence
+
+class simple_alu_seq extends uvm_sequence #(t_data);
+
+    `uvm_object_utils(simple_alu_seq)
+
+    function new(string name="");
+        super.new(name);
+    endfunction
+
+    task body();
+        t_data data;
+        start_item(data);
+        //assert(data.randomize());
+        finish_item(data);
+        //`uvm_do(data);
+    endtask
+endclass
+
+
+//==============================================================================
+// test
+
+class simple_alu_test extends uvm_test;
+
+    `uvm_component_utils(simple_alu_test)
+
+    env m_env;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        m_env = env::type_id::create("env", this);
+    endfunction
+
+    virtual function void end_of_elaboration_phase (uvm_phase phase);
+        uvm_top.print_topology();
+    endfunction
+
+
+    task run_phase(uvm_phase phase);
+        simple_alu_seq seq;
+        `uvm_info("test", "In the `run` phase", UVM_LOW);
+        phase.raise_objection(this);
+        seq = simple_alu_seq::type_id::create("seq1");
+        seq.start(m_env.m_seqr);
+        phase.drop_objection(this);
+    endtask
+endclass
 
 //==============================================================================
 // testbench
@@ -97,7 +170,6 @@ endclass
 module simple_alu_tb;
 
 bit clk;
-env env;
 simple_alu_in_if in_if(.clk(clk));
 
 simple_alu DUT (
@@ -116,10 +188,9 @@ initial begin: proc_main
     `uvm_info("main", "Testbench starting (msg verbosity: DEBUG)", UVM_DEBUG);
     `uvm_info("main", "Testbench starting (msg verbosity: NONE)", UVM_NONE);
 
-    env = new("env");
     uvm_resource_db#(virtual simple_alu_in_if)::set("env", "simple_alu_in_if", in_if);
 
-    run_test();
+    run_test("simple_alu_test");
 end
 
 endmodule
